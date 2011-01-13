8a278db2a263b5fa09e70413598b89ec2956b0fa
kimei <kim@hvafor.no>
2011-01-13 15:39:15 +0100

    la til vhdl

---
+++ VHDL/VHDL.vhd
@@ -0,0 +1,40 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    15:23:22 01/13/2011 
+-- Design Name: 
+-- Module Name:    VHDL - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+---- Uncomment the following library declaration if instantiating
+---- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity VHDL is
+    Port ( Jada : in  STD_LOGIC);
+end VHDL;
+
+architecture Behavioral of VHDL is
+
+begin
+
+
+end Behavioral;
+
